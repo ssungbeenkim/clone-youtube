1d91c92aaabc617605a37c13637b7e89071bb28b
wandakim <nwd1583@naver.com>
2023-05-13 14:55:53 +0900

    Add axios

--- src/page/Videos.jsx
+++ src/page/Videos.jsx
@@ -2,6 +2,8 @@ import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import { useParams } from "react-router-dom";
 import VideoCard from "../components/VideoCard";
+import axios from "axios";
+
 export default function Videos() {
   const { keyword } = useParams();
   const {
@@ -9,9 +11,12 @@ export default function Videos() {
     error,
     data: videos,
   } = useQuery(["videos", keyword], async () => {
-    return fetch(`/videos/${keyword ? "search" : "popular"}.json`)
-      .then((res) => res.json())
-      .then((data) => data.items);
+    return axios
+      .get(`/videos/${keyword ? "search" : "popular"}.json`)
+      .then((res) => {
+        console.log(res);
+        return res.data.items;
+      });
   });
   return (
     <>
