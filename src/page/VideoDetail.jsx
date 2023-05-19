e458a07c82604f47cf20adedeff84b39c83fcdb6
wandakim <nwd1583@naver.com>
2023-05-19 14:55:59 +0900

    Detail page framing

--- src/page/VideoDetail.jsx
+++ src/page/VideoDetail.jsx
@@ -1,5 +1,34 @@
 import React from "react";
-
+import { useLocation } from "react-router-dom";
+import ChannelIInfo from "../components/ChannelInfo";
+import RelatedVideos from "../components/RelatedVideos";
 export default function VideoDetail() {
-  return <div>Details</div>;
+  const {
+    state: { video },
+  } = useLocation();
+  console.log(video);
+  const { title, channelId, channelTitle, description } = video.snippet;
+  return (
+    <section>
+      <article>
+        <iframe
+          title={title}
+          id="player"
+          type="text/html"
+          width="100%"
+          height="649"
+          src={`http://www.youtube.com/embed/${video.id}`}
+          frameBorder="0"
+        />
+        <div>
+          <h2>{title}</h2>
+          <ChannelIInfo id={channelId} name={channelTitle} />
+          <pre>{description}</pre>
+        </div>
+      </article>
+      <section>
+        <RelatedVideos id={video.id} />
+      </section>
+    </section>
+  );
 }
